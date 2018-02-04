f7f9a01df52cfef4e08e6edddf5dacbe957f570c
Sai Prasad <saiprasad1996@hotmail.com>
2018-02-04 19:29:15 +0530

    minor bug fix

--- entry.py
+++ entry.py
@@ -3,6 +3,7 @@ import views.Reports
 import views.salespage
 
 if __name__ == "__main__":
-    views.salespage.vp_start_gui()
+    # views.salespage.vp_start_gui()
     # views.additem.vp_start_gui()
     # views.Reports.vp_start_gui()
+    views.salespage.SalesPage()
