1ac6946d14318cf941503d6c53614a2cdda65abf
Sai Prasad <saiprasad1996@hotmail.com>
2018-02-18 18:44:23 +0530

    Kivy reports

--- entry.py
+++ entry.py
@@ -1,10 +1,11 @@
-
 import views.additem
 import views.Reports
 import views.salespage
 import views.add_item
 import views.main_page
 import views.categories_kivy
+import views.reports_kivy
+
 if __name__ == "__main__":
     # views.salespage.vp_start_gui()
     # views.additem.vp_start_gui()
@@ -12,4 +13,5 @@ if __name__ == "__main__":
     # views.salespage.SalesPage()
     # views.add_item.AddItems(category="shampoo").run()
     # views.main_page.SalesPage().run()
-    views.categories_kivy.Categories().run()
+    # views.categories_kivy.Categories().run()
+    views.reports_kivy.Reports().run()
