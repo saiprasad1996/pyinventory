783ffccf1513d2e2cc5046da1a8cdfad0893dcb2
Sai Prasad <saiprasad1996@hotmail.com>
2018-03-16 22:43:03 +0530

    Mail configuration backend

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
-# For mailing
+import os, json
+
+#  For mailing
 # From and to mails are same
 EMAIL = ""
 MAIL_PASSWORD = ""
@@ -15,3 +17,35 @@ DB_PORT = 3306
 
 STOCK_LIMIT = 5
 COMPUTER_ID = "C1"
+
+current_dir = os.path.dirname(__file__)
+# print(current_dir)
+config_file = os.path.join(current_dir, "config.json")
+
+config = ""
+try:
+    with open(config_file) as f:
+        config = f.read()
+
+    config = json.loads(config)
+
+    # From and to mails are same
+    EMAIL = config["email"]
+    MAIL_PASSWORD = config["mail_password"]
+    MAIL_HOST = config["mail_host"]
+    MAIL_PORT = int(config["mail_port"])  # This should be a number
+
+    # Database Configuration
+
+    DB_NAME = config["db_name"]
+    DB_PASSWORD = config["db_password"]
+    DB_USER = config["db_user"]
+    DB_HOST = config["db_host"]
+    DB_PORT = int(config["db_port"])
+
+    STOCK_LIMIT = int(config["stock_limit"])
+    COMPUTER_ID = config["computer_id"]
+
+except:
+    print("An exception occured while reading the file")
+
--- views/settings_page.py
+++ views/settings_page.py
@@ -6,6 +6,10 @@ from kivy.graphics import Color, Rectangle
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
+import json
+from backend import config
+from backend.utils import messagebox
+
 
 class SettingsPage(FloatLayout):
     def __init__(self, **kwargs):
@@ -32,22 +36,22 @@ class SettingsPage(FloatLayout):
                   pos_hint={'center_x': 0.175, 'center_y': 0.87})
         )
         textinput_password = TextInput(hint_text='Password',
-                                    multiline=False,
-                                    password=True,
-                                    pos_hint={'center_x': 0.43, 'center_y': 0.87},
-                                    size_hint=(0.35, 0.055))
+                                       multiline=False,
+                                       password=True,
+                                       pos_hint={'center_x': 0.43, 'center_y': 0.87},
+                                       size_hint=(0.35, 0.055))
         root.add_widget(textinput_password)
 
         root.add_widget(Label(text='Mail Server',
-                            color=(0, 0, 0, 0.8),
-                            font_size=16,
-                            pos_hint={'center_x': 0.175, 'center_y': 0.79})
-        )
+                              color=(0, 0, 0, 0.8),
+                              font_size=16,
+                              pos_hint={'center_x': 0.175, 'center_y': 0.79})
+                        )
 
         textinput_mailserver = TextInput(hint_text='mail.example.com',
-                                    multiline=False,
-                                    pos_hint={'center_x': 0.43, 'center_y': 0.79},
-                                    size_hint=(0.35, 0.055))
+                                         multiline=False,
+                                         pos_hint={'center_x': 0.43, 'center_y': 0.79},
+                                         size_hint=(0.35, 0.055))
         root.add_widget(textinput_mailserver)
 
         root.add_widget(
@@ -57,9 +61,9 @@ class SettingsPage(FloatLayout):
                   pos_hint={'center_x': 0.175, 'center_y': 0.71})
         )
         textinput_port = TextInput(hint_text='Port',
-                                    multiline=False,
-                                    pos_hint={'center_x': 0.43, 'center_y': 0.71},
-                                    size_hint=(0.35, 0.055))
+                                   multiline=False,
+                                   pos_hint={'center_x': 0.43, 'center_y': 0.71},
+                                   size_hint=(0.35, 0.055))
         root.add_widget(textinput_port)
 
         donebtn = Button(text='Submit',
@@ -67,11 +71,25 @@ class SettingsPage(FloatLayout):
                          pos_hint={'center_x': 0.5, 'center_y': 0.2})
 
         def submit(instance):
+            if len(textinput_mailserver.text) == 0 or len(textinput_password.text) == 0 or len(
+                    textinput_email.text) == 0 or len(textinput_port.text) == 0:
+                messagebox(title="Error", message="Please fill in all the details")
             mailsever = textinput_mailserver.text
             password = textinput_password.text
             email = textinput_email.text
             port = textinput_port.text
-            print(mailsever,password,email,port)
+
+            config_vals = config.config
+            print(mailsever, password, email, port)
+            with open(config.config_file,mode='w') as f:
+                config_vals["email"] = email
+                config_vals["mail_host"] = mailsever
+                config_vals["mail_password"] = password
+                config_vals["mail_port"] = port
+                stringify = json.dumps(config_vals)
+                f.write(stringify)
+                messagebox(title="Success",message="Configuration written successfully")
+
         donebtn.bind(on_press=submit)
         root.add_widget(donebtn)
 
